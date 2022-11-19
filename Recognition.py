2c666feac2d7bc14c9e811c642e54f9ff55e5992
Sam Taubman <taubmansam@gmail.com>
2022-11-18 20:31:00 -0500

    Add files via upload

---
+++ Recognition.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+import keras
+
+import matplotlib.pyplot as plt
+
+ 
+
+#Importing the image dataset
+
+from keras.datasets import mnist
+
+#Loaded data
+#I love butt sex
+# kill me and put my dick in my butt
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+plt.imshow(x_train[0])
+plt.show()
